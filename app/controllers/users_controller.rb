de6e9969ab62e9ee8187f8846b04cc5af80dc2e5
sarahbes <sarah.besnainou@gmail.com>
2015-06-29 10:17:27 +0200

    finished bar app Sarah

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,21 @@ def create
     redirect_to bar_path(@bar)
   end
   
+  def edit 
+    @bar = Bar.find(params[:bar_id])
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @bar = Bar.find(params[:bar_id])
+    @user = @bar.users.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @bar
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :phone_number, :time, :notes)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   belongs_to :bar
+  validates :name, :time, presence: true
 end
